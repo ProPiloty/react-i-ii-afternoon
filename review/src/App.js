1878e73bb00260f7ef6fe2a66e8707ae4054fb5b
Tyler Ellingford <tyler@Rickys-MacBook-Pro.local>
2019-05-02 19:33:12 -0600

    saving progress

---
+++ review/src/App.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import './reset.css';
+import './App.css';
+import users from './data';
+
+//Components
+import Navbar from './Components/Navbar/Navbar';
+import Card from './Components/Card/Card';
+import Controls from './Components/Controls/Controls';
+
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      users: users,
+      page: 1,
+    }
+  }
+
+  next = () => {
+    if(this.state.page === 25) {
+      this.setState({
+        page: 1,
+      })
+    } else {
+      this.setState({
+        page: this.state.page + 1,
+      })
+    }
+  }
+
+  prev = () => {
+    if(this.state.page === 1) {
+      this.setState({
+        page: 25,
+      })
+    } else {
+      this.setState({
+        page: this.state.page - 1,
+      })
+    }
+  }
+
+  render(){
+    const filteredUser = this.state.users.filter(user => (
+      user.id === this.state.page
+    ))
+
+    return (
+      <div className='app-container'>
+        <Navbar/>
+        <Card filteredUser={filteredUser} />
+        <Controls next={this.next} prev={this.prev} />
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ review/src/Components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Navbar.css';
+
+export default function Navbar(){
+    return (
+        <div className='navbar-container'>
+            <h1>Home</h1>
+        </div>
+    )
+}
